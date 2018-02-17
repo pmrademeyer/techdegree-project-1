ee15aba26fa001dc86994a353539d762bd5c9325
Peet Rademeyer <pmrademeyer@gmail.com>
2018-02-17 18:07:52 +0800

    Created Quotes Arrat

--- js/script.js
+++ js/script.js
@@ -2,3 +2,11 @@
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+var quotes = [
+  { quote : "This is the first quote", source : "Person's name" },
+  { quote : "this is the second quote", source : "Person's quote" },
+  { quote : "this is the third quote", source : "Person's name" },
+  { quote : "this is the fourth quote", source : "Person's name" },
+  { quote : "this is the fifth quote", source : "Person's name", citation : "this is where the quote comes from", year : "Year of the quote" }
+];
+console.log("123");
