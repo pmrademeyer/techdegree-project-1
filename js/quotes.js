4539896391f3bc9c2820942efd39c3b741e3a670
Peet Rademeyer <pmrademeyer@gmail.com>
2018-02-20 19:28:17 +0800

    Random Number Generator Function

--- js/quotes.js
+++ js/quotes.js
@@ -1,7 +1,3 @@
-// event listener to respond to "Show another quote" button clicks
-// when user clicks anywhere on the button, the "printQuote" function is called
-//document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
 var quotes = [
   { quote : "This is the first quote", source : "Person's name" },
   { quote : "this is the second quote", source : "Person's quote" },
@@ -9,11 +5,3 @@ var quotes = [
   { quote : "this is the fourth quote", source : "Person's name" },
   { quote : "this is the fifth quote", source : "Person's name", citation : "this is where the quote comes from", year : "Year of the quote" }
 ];
-
-
-function getRandomNumber ( lower, upper ) {
-  return Math.floor( Math.random() * (quotes.length)) + 1;
-}
-
-console.log(getRandomNumber ( ));
-console.log(quotes);
